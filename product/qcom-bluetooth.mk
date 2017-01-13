e31b9443866edb547ce1477d4685e6c4747e6bca
Myles Watson <mylesgw@google.com>
2017-01-13 11:47:17 -0800

    marmite: Build the Bluetooth HAL
    Test: VTS test passes on Marlin, Bluetooth starts/stops
    Change-Id: Ib1a1849c060c39e88c0219cb8421e3426cd3e7ac

--- product/qcom-bluetooth.mk
+++ product/qcom-bluetooth.mk
@@ -2,3 +2,8 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
