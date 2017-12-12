26fe8da5d90715f881c93f657ffb38d773d928fa
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-12 16:26:00 +0300

    marmite: Switch to AOSP's pn5xx NFC stack
    Change-Id: I3ed20d3a764a97841dbddef6b9757545b0c5e187

--- product/nfc.mk
+++ product/nfc.mk
@@ -1,13 +1,9 @@
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
     com.android.nfc_extras \
-    com.nxp.nfc.nq \
-    libnqnfc-nci \
-    libp61-jcop-kit \
-    nfc_nci.nqx.default \
-    NQNfcNci \
-    nqnfcee_access.xml \
-    nqnfcse_access.xml \
+    nfc_nci.msm8937 \
+    NfcNci \
     Tag
 
 # NFC
