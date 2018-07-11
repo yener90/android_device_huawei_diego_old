e373288a5e3db25e0cfc6bc96708291017f40cdc
ASAZING <malcolmedina206@gmail.com>
2018-07-10 21:39:43 -0500

    Diego: Update kernel

--- board/kernel.mk
+++ board/kernel.mk
@@ -13,5 +13,5 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
-TARGET_KERNEL_SOURCE := kernel/wileyfox/msm8937
-TARGET_KERNEL_CONFIG := lineageos_marmite_defconfig
+TARGET_KERNEL_CONFIG := lineage_diego_defconfig
+TARGET_KERNEL_SOURCE := kernel/huawei/msm8937
